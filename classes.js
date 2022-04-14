7b418d6c4721f09016a0bf4f22ac7b0750f6c31d
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-04-14 19:27:38 -0400

    Initial commit

---
+++ classes.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class MainProperty {
+    constructor(src, title, reviews) {
+        this.src = src;
+        this.title = title;
+        this.reviews = reviews;
+    }
+}
+exports.default = MainProperty;
