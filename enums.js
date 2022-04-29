80ee8749ac38f9e421afeb03f037d25c1e2acad6
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-04-28 23:43:57 -0400

    debugging

--- enums.js
+++ enums.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.LoyaltyUser = exports.Permissions = void 0;
 var Permissions;
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getTopTwoReviews = exports.makeMultiple = exports.showDetails = exports.populateUser = exports.showReviewTotal = void 0;
 const reviewTotalDisplay = document.querySelector('#reviews');
